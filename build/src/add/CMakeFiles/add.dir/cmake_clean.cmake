44effc8ad80e5519f484f770e349a6bb0da01f93
Response <Hjcresponse@qq.com>
2023-12-25 12:05:16 +0800

    push a iterm

---
+++ build/src/add/CMakeFiles/add.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/add.dir/add.cpp.o"
+  "CMakeFiles/add.dir/add3.cpp.o"
+  "../../../lib/libadd.pdb"
+  "../../../lib/libadd.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/add.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/src/main/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/hjc/work/learnC++/myStudy/src/main/main.cpp" "/home/hjc/work/learnC++/myStudy/build/src/main/CMakeFiles/main.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src/main/../add"
+  "../src/main/../sub"
+  "../src/main/../mul"
+  "../src/main/../main"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/hjc/work/learnC++/myStudy/build/src/add/CMakeFiles/add.dir/DependInfo.cmake"
+  "/home/hjc/work/learnC++/myStudy/build/src/sub/CMakeFiles/sub.dir/DependInfo.cmake"
+  "/home/hjc/work/learnC++/myStudy/build/src/mul/CMakeFiles/mul.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
